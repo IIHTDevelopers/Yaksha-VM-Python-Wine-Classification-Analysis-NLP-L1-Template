31573115692bdc15566be64448dddc0395f7bcf4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-09-30 12:07:39 +0530

    Update TestUtils.py

--- test/TestUtils.py
+++ test/TestUtils.py
@@ -9,7 +9,7 @@ class TestUtils:
 
     @classmethod
     def yakshaAssert(self, test_name, result, test_type):
-        ref = open("custom.ih", "r")
+        ref = open("../custom.ih", "r")
         customData = ref.read()
         ref.close()
         test_case_results = dict()
